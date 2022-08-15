9bd9e73be5fd9070700d2db1dfa67f6a8612b932
Christofer Guimarães <christoferguima@gmail.com>
2022-08-15 01:17:02 -0300

    email consumer created

---
+++ src/main/java/com/guimaraes/email/consumer/EmailConsumer.java
@@ -0,0 +1,27 @@
+package com.guimaraes.email.consumer;
+
+import com.guimaraes.email.dto.EmailDto;
+import com.guimaraes.email.entity.EmailEntity;
+import com.guimaraes.email.service.EmailService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailConsumer {
+
+    EmailService emailService;
+
+    EmailConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @RabbitListener(queues = "${spring.rabbitmq.queue}")
+    public void listen(@Payload EmailDto emailDto) {
+        EmailEntity emailModel = new EmailEntity();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendEmail(emailModel);
+        System.out.println("Email status: " + emailModel.getStatusEmail().toString());
+    }
+}
